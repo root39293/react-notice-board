ede09b791f46c3f4eef9cb3c69ad339a799e5447
toujun <root39293@gmail.com>
2023-05-18 09:28:12 +0900

    app.js edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-import React, { useState } from 'react';
-import { ChakraProvider, Box, Heading, Text, Flex, VStack, Divider, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, ModalCloseButton, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import React, { useState } from "react";
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  Text,
+  Flex,
+  VStack,
+  Divider,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function PostList() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'First Post', author: 'John Doe', date: 'May 18, 2023', content: 'hello world!' },
-    { id: 2, title: 'Second Post', author: 'Jane Smith', date: 'May 19, 2023', content: 'hello there' },
-    { id: 3, title: 'Third Post', author: 'Michael Johnson', date: 'May 20, 2023', content: 'hey!' },
+    {
+      id: 1,
+      title: "First Post",
+      author: "John Doe",
+      date: "May 18, 2023",
+      content: "hello world!",
+      likes: 0,
+    },
+    {
+      id: 2,
+      title: "Second Post",
+      author: "Jane Smith",
+      date: "May 19, 2023",
+      content: "hello there",
+      likes: 0,
+    },
+    {
+      id: 3,
+      title: "Third Post",
+      author: "Michael Johnson",
+      date: "May 20, 2023",
+      content: "hey!",
+      likes: 0,
+    },
   ]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [newPostTitle, setNewPostTitle] = useState('');
-  const [newPostAuthor, setNewPostAuthor] = useState('');
-  const [newPostContent, setNewPostContent] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
+  const [newPostTitle, setNewPostTitle] = useState("");
+  const [newPostAuthor, setNewPostAuthor] = useState("");
+  const [newPostContent, setNewPostContent] = useState("");
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -19,9 +58,9 @@ function PostList() {
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    setNewPostTitle('');
-    setNewPostAuthor('');
-    setNewPostContent('');
+    setNewPostTitle("");
+    setNewPostAuthor("");
+    setNewPostContent("");
   };
 
   const handleAddPost = () => {
@@ -31,37 +70,53 @@ function PostList() {
       author: newPostAuthor,
       date: new Date().toLocaleDateString(),
       content: newPostContent,
+      likes: 0,
     };
 
     setPosts([...posts, newPost]);
     handleCloseModal();
   };
 
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
+  const handleLikePost = (postId) => {
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes + 1 };
+      }
+      return post;
+    });
 
-  const filteredPosts = posts.filter((post) => {
-    const searchTermWithoutWhitespace = searchTerm.trim().toLowerCase();
-    const postTitleWithoutWhitespace = post.title.trim().toLowerCase();
-    const postAuthorWithoutWhitespace = post.author.trim().toLowerCase();
-    return(
-    postTitleWithoutWhitespace.includes(searchTermWithoutWhitespace) ||
-    postAuthorWithoutWhitespace.includes(searchTermWithoutWhitespace)
-    );
-  });
+    setPosts(updatedPosts);
+  };
 
   return (
     <VStack spacing={4} align="stretch">
-      <Flex justify="flex-end" w="100%">
-        <FormControl w="200px" bg="#ffffff">
-          <Input placeholder="search..." value={searchTerm} onChange={handleSearch} />
-        </FormControl>
-      </Flex>
-      {filteredPosts.map((post) => (
-        <PostItem key={post.id} post={post} />
+      {posts.map((post) => (
+        <Box
+          key={post.id}
+          p={4}
+          shadow="md"
+          borderWidth="1px"
+          borderRadius="md"
+          bg="#FFFFFF"
+        >
+          <Heading as="h3" size="md" mb={2}>
+            {post.title}
+          </Heading>
+          <Text fontSize="sm" color="gray.500" mb={2}>
+            By {post.author} on {post.date}
+          </Text>
+          <Divider />
+          <Flex justify="space-between" mt={4}>
+            <Button colorScheme="blue" onClick={() => handleLikePost(post.id)}>
+              ❤ {post.likes}
+            </Button>
+            <Button colorScheme="blue" onClick={() => handleOpenModal(post.id)}>
+              Read More
+            </Button>
+          </Flex>
+        </Box>
       ))}
-      <Button colorScheme="teal" onClick={handleOpenModal} alignSelf="flex-end">
+      <Button colorScheme="blue" onClick={handleOpenModal} alignSelf="flex-end">
         글쓰기
       </Button>
 
@@ -73,19 +128,32 @@ function PostList() {
           <ModalBody>
             <FormControl>
               <FormLabel>제목</FormLabel>
-              <Input value={newPostTitle} onChange={(e) => setNewPostTitle(e.target.value)} />
+              <Input
+                value={newPostTitle}
+                onChange={(e) => setNewPostTitle(e.target.value)}
+              />
             </FormControl>
             <FormControl mt={4}>
               <FormLabel>작성자</FormLabel>
-              <Input value={newPostAuthor} onChange={(e) => setNewPostAuthor(e.target.value)} />
+              <Input
+                value={newPostAuthor}
+                onChange={(e) => setNewPostAuthor(e.target.value)}
+              />
             </FormControl>
             <FormControl mt={4}>
               <FormLabel>내용</FormLabel>
-              <Input value={newPostContent} onChange={(e) => setNewPostContent(e.target.value)} />
+              <Input
+                value={newPostContent}
+                onChange={(e) => setNewPostContent(e.target.value)}
+              />
             </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="teal" onClick={handleAddPost} isDisabled={!newPostTitle || !newPostAuthor || !newPostContent}>
+            <Button
+              colorScheme="blue"
+              onClick={handleAddPost}
+              isDisabled={!newPostTitle || !newPostAuthor || !newPostContent}
+            >
               추가하기
             </Button>
             <Button colorScheme="gray" ml={2} onClick={handleCloseModal}>
@@ -95,49 +163,6 @@ function PostList() {
         </ModalContent>
       </Modal>
     </VStack>
-  );
-}
-
-function PostItem({ post }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  return (
-    <Box p={4} shadow="md" borderWidth="1px" borderRadius="md" bg="#FFFFFF">
-      <Heading as="h3" size="md" mb={2}>
-        {post.title}
-      </Heading>
-      <Text fontSize="sm" color="gray.500" mb={2}>
-        By {post.author} on {post.date}
-      </Text>
-      <Divider />
-      <Button colorScheme="teal" onClick={handleOpenModal}>
-        Read More
-      </Button>
-
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{post.title}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text>{post.content}</Text>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="teal" onClick={handleCloseModal}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Box>
   );
 }
 
@@ -154,7 +179,8 @@ function App() {
               fontSize="3xl"
               fontWeight="bold"
               color="black"
-              fontFamily="Roboto, sans-serif">
+              fontFamily="Roboto, sans-serif"
+            >
               Board
             </Heading>
             <PostList />
@@ -166,4 +192,3 @@ function App() {
 }
 
 export default App;
-
